64784b83597a7af5ee791f170076a151432d9943
Ekaterina Orlova <katyaorl@gmail.com>
2019-05-20 10:50:02 +0300

    creating docs for gh-pages

---
+++ docs/precache-manifest.71f03133a2ceb5a5b9e1e6ec345cc087.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26972bad0b8d5232c65425504998efa5",
+    "url": "/ut-test/index.html"
+  },
+  {
+    "revision": "643b6802cd61e020b1b2",
+    "url": "/ut-test/static/css/main.adc6eb48.chunk.css"
+  },
+  {
+    "revision": "dc483b10911da1802b4d",
+    "url": "/ut-test/static/js/2.fe7d82aa.chunk.js"
+  },
+  {
+    "revision": "643b6802cd61e020b1b2",
+    "url": "/ut-test/static/js/main.82c9971a.chunk.js"
+  },
+  {
+    "revision": "b6b1f79a2a3ede076737",
+    "url": "/ut-test/static/js/runtime~main.b5510673.js"
+  },
+  {
+    "revision": "5cb3800382f2d5c8db0834a3a8a6541f",
+    "url": "/ut-test/static/media/company2.5cb38003.png"
+  },
+  {
+    "revision": "3f01acf89fd85466b68d63ce81ab0265",
+    "url": "/ut-test/static/media/company4.3f01acf8.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ut-test/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "851ec842c8061879d0b1cc5d8ea19de5",
+    "url": "/ut-test/static/media/main-bg.851ec842.jpg"
+  }
+]);
